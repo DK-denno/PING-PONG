f04d80bf3784bb805fd312f22e21b21143d520bc
dk-denno <dennisveer27@gmail.com>
2018-07-13 16:19:47 +0300

    added the new have that is the business logic

--- js/ping.js
+++ js/ping.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
     $('#display').empty();
   }
 }
+
+var No=parseInt($("form#input").val());
+var output=[];
+for (var initial=1; initial<= No; initial++ );{
+  if(index % 15===0) {
+    output.push("pingpong");
+    }
+    else if (index % 5===0) {
+    output.push("pong");
+    }
+     else if (index % 3===0) {
+    output.push("ping");
+    }
+    else{
+    output.push("index");
+    }
