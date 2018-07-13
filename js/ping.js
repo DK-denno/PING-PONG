587a6c6af3275bb9cd51a2ab877d9d605997fef7
dk-denno <dennisveer27@gmail.com>
2018-07-13 17:21:13 +0300

    added both user interface and business logic

--- js/ping.js
+++ js/ping.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
-  $("form#play").click(function(event){
+
+  $("form").submit(function(event){
     event.preventDefault();
     $('#display').empty();
-  }
-}
 
-var No=parseInt($("form#input").val());
-var output=[];
-for (var initial=1; initial<= No; initial++ );{
-  if(initial % 15===0) {
-    output.push("pingpong");
+  });
+  for (var initial=1; initial<= No; initial++ ){
+    if(initial % 15===0) {
+      output.push("pingpong");
+      }
+      else if (initial % 5===0) {
+      output.push("pong");
+      }
+       else if (initial % 3===0) {
+      output.push("ping");
+      }
+      else{
+      output.push(initial);
+      }
+      console.log(No);
+      //return no;
     }
-    else if (initial % 5===0) {
-    output.push("pong");
-    }
-     else if (initial % 3===0) {
-    output.push("ping");
-    }
-    else{
-    output.push("index");
-    }
-    console.log(no)
-    return no
+
+});
+});
